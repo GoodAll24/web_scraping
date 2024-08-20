7a99409d94ec3312baef0f90f77a3731d2094dd8
aldenr.gonzalezp <110749060+GoodAll24@users.noreply.github.com>
2024-08-20 18:15:07 +0000

    update 20-8-24

--- app.js
+++ app.js
@@ -5,85 +5,122 @@
 // const { executablePath } = require("puppeteer");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { intern } = require('./helpers/intern.js');
-const { add_media, add_media_data } = require('./helpers/urls');
-
-
+// const { add_media, add_media_data } = require('./helpers/urls');
+const { onCheck } = require("./helpers/urls");
+const { channel } = require("diagnostics_channel");
 
 // puppeteer.use(StealthPlugin());
 
-
 const main = async () => {
+  // const art1 = {
+  //   "ext": false,
+  //   "link": "a",
+  //   "main": "article.standard-entry-box.category-yellow ",
+  //   "title": "h2.entry-title",
+  //   "image": "picture > img",
+  //   "content": "",
+  //   "extImg": false,
+  //   // "cssImage": true,
+  //   // "cssImageAttr": "data-img-url",
+  //   // "cssImageExtraText": false,
+  // };
 
-  const art1 = {
-    "ext": false,
-    "link": "a",
-    "main": "article.standard-entry-box.category-yellow ",
-    "title": "h2.entry-title",
-    "image": "picture > img",
-    "content": "",
-    "extImg": false
-    // "cssImage": true,
-    // "cssImageAttr": "data-img-url",
-    // "cssImageExtraText": false,
-  };
-
-  const url = "https://www.lacapital.com.ar/secciones/zoom.html";
+  // const url = "https://www.lacapital.com.ar/secciones/zoom.html";
   const datos = [];
-  console.log("Getting page...");
-  await axios.get(url)
-    .then(response => {
+  // for (let page of onCheck){
+  const page = onCheck[1];
+  console.log("Getting page", page.url, "...");
+  await axios
+    .get(page.url)
+    .then((response) => {
       console.log("Page loaded!");
       const $ = cheerio.load(response.data);
 
       // -----Getting articles----- //
       // console.log($(art1["main"]).html());
 
-      $(art1["main"])
-        .each((i, element) => {
-          // console.log($(element).html());
-          datos.push({});
-          let long = datos.length - 1;
-          datos[long].title = art1["title"] ? `${$(element).find(art1["title"]).text()}`.trim() : null;
-          datos[long].link = art1["link"] ? art1["ext"] ? `${url}${$(element).find(art1["link"]).attr("href")}` : $(element).find(art1["link"]).attr("href") : null;
-          datos[long].content = art1["content"] ? $(element).find(art1["content"]).text() : null;
-          datos[long].image = art1["image"]
-            ? art1["cssImage"]
-              ? art1["cssImageExtraText"]
-                ? art1["cssImageAttr"] === "style"
-                  ? `${$(element).find(art1["image"]).attr(art1["cssImageAttr"])}`.slice(23, -3)
-                  : `${$(element).find(art1["image"]).attr(art1["cssImageAttr"])}`.slice(5, -2)
-                : $(element).find(art1["image"]).attr(art1["cssImageAttr"])
-              : art1["extImg"]
-                ? `${art1["media"]}/${$(element).find(art1["image"]).attr("src")}`
-                : $(element).find(art1["image"]).attr("src") : null;
-          datos[long].publishedAt = new Date();
-          datos[long].pubDate = new Date();
-          datos[long].likes = 0;
-          datos[long].dislikes = 0;
-          datos[long].state = "pendiente";
-        });
+      $(page["main"]).each((i, element) => {
+        // console.log($(element).html());
+        datos.push({});
+        let long = datos.length - 1;
+        datos[long].title = page["title"]
+          ? `${$(element).find(page["title"]).text()}`.trim()
+          : null;
+        datos[long].link = page["link"]
+          ? page["ext"]
+            ? `${url}${$(element).find(page["link"]).attr("href")}`
+            : $(element).find(page["link"]).attr("href")
+          : null;
+        datos[long].content = page["content"]
+          ? $(element).find(page["content"]).text()
+          : null;
+        datos[long].image = page["image"]
+          ? page["cssImage"]
+            ? page["cssImageExtraText"]
+              ? page["cssImageAttr"] === "style"
+                ? `${$(element).find(page["image"]).attr(page["cssImageAttr"])}`.slice(
+                    23,
+                    -3,
+                  )
+                : `${$(element).find(page["image"]).attr(page["cssImageAttr"])}`.slice(
+                    5,
+                    -2,
+                  )
+              : $(element).find(page["image"]).attr(page["cssImageAttr"])
+            : page["extImg"]
+              ? `${page["media"]}/${$(element).find(page["image"]).attr("src")}`
+              : $(element).find(page["image"]).attr("src")
+          : null;
+        datos[long].publishedAt = new Date();
+        datos[long].pubDate = new Date();
+        datos[long].likes = 0;
+        datos[long].dislikes = 0;
+        datos[long].state = "pendiente";
+      });
       console.log(datos);
     })
-    .catch(error => console.log("Error with page --> ", url, "\n >>>", error.code || error))
+    .catch((error) =>
+      console.log(
+        "Error with page --> ",
+        page.url,
+        "\n >>>",
+        error.code || error,
+      ),
+    )
     .finally(() => console.log("Done! \n\n\n"));
 
   // -----Getting metadata----- //
-  const parsedEnlace = new URL(url);
+  const parsedEnlace = new URL(page.url);
   console.log("Getting metadata from: ", parsedEnlace.hostname, "...");
-  await axios.get(`${parsedEnlace.protocol}//${parsedEnlace.hostname}`)
-    .then(response => {
+  await axios
+    .get(`${parsedEnlace.protocol}//${parsedEnlace.hostname}`)
+    .then((response) => {
       console.log("Metadata obtained!");
       const metaPage = cheerio.load(response.data);
-      console.log("Medio >>>>>> ", metaPage("title").text());
-      const pageIcon = metaPage('link[rel="icon"]').attr("href")
-        || metaPage('link[rel="shortcut icon"]').attr("href")
-        || "No hay ícono para este sitio";
-
-      console.log(pageIcon);
+      // console.log("Medio >>>>>> ", metaPage("title").text());
+      const pageIcon =
+        metaPage('link[rel="icon"]').attr("href") ||
+        metaPage('link[rel="shortcut icon"]').attr("href") ||
+        "No hay ícono para este sitio";
+      
+      const data2Fill = {
+        url: page.url,
+        channel: metaPage("title").text(),
+        link_logo: pageIcon,
+        access: JSON.stringify(page),
+      };
+      console.log(data2Fill);
     })
-    .catch(error => console.log("Error with metadata --> ", url, "\n >>>", error.code || error))
+    .catch((error) =>
+      console.log(
+        "Error with metadata --> ",
+        page.url,
+        "\n >>>",
+        error.code || error,
+      ),
+    )
     .finally(() => console.log("Meta proccess Done!"));
+  // }
 };
 
 main();
--- helpers/urls.js
+++ helpers/urls.js
@@ -12,103 +12,103 @@ const mexico = [
   "https://www.elsiglodetorreon.com.mx/seccion/espectaculos", // done
 ];
 const usa = [
-  'https://www.billboard.com/c/espanol/noticias/', // problemas de carga (revisar en server)
-  'https://www.billboard.com/c/espanol/musica/', // problemas de carga (revisar en server)
-  'https://musicaislife.com/musica-news/', // done (tema imagen)
-  'https://prensadehouston.com/category/entretenimiento/', // done (imagen en source ("srcset"))
-  'https://wowlarevista.com/category/musica/', // security issues
-  'https://www.telemundo.com/entretenimiento', // done * (aquí hay que revisar si las noticias con video se pueden integrar directamente a la Tele Revista) 
-  'https://cnnespanol.cnn.com/category/musica/', // done *(aquí hay que revisar si las noticias con video se pueden integrar directamente a la Tele Revista)
-  'https://www.latimes.com/espanol/etiqueta/musica', // done
-  'https://www.hispanicpost.com/category/entretenimiento/musica-tv-y-cine/', // error 404
-  'https://somoslarevistausa.com/category/musica/', // done
-  'https://lakw.us/category/musica/', // done
-  'https://www.telemundoamarillo.com/entretenimiento/', // done
-  'https://www.mundodeportivo.com/us/estrellas-latinas', // done
-  'https://www.noticiany.com/category/entretenimiento/', // done
-  'https://es.rollingstone.com/categoria/musica/noticias-musica/',
-  'https://www.notistarz.com/categorias/musica/', // tema imagen
-  'https://efe.com/noticias/musica/', // done
-  'https://ellatinodigital.com/categoria/secciones/farandula/', // done
-  'https://www.latingrammy.com/noticias', // error de carga
+  "https://www.billboard.com/c/espanol/noticias/", // problemas de carga (revisar en server)
+  "https://www.billboard.com/c/espanol/musica/", // problemas de carga (revisar en server)
+  "https://musicaislife.com/musica-news/", // done (tema imagen)
+  "https://prensadehouston.com/category/entretenimiento/", // done (imagen en source ("srcset"))
+  "https://wowlarevista.com/category/musica/", // security issues
+  "https://www.telemundo.com/entretenimiento", // done * (aquí hay que revisar si las noticias con video se pueden integrar directamente a la Tele Revista)
+  "https://cnnespanol.cnn.com/category/musica/", // done *(aquí hay que revisar si las noticias con video se pueden integrar directamente a la Tele Revista)
+  "https://www.latimes.com/espanol/etiqueta/musica", // done
+  "https://www.hispanicpost.com/category/entretenimiento/musica-tv-y-cine/", // error 404
+  "https://somoslarevistausa.com/category/musica/", // done
+  "https://lakw.us/category/musica/", // done
+  "https://www.telemundoamarillo.com/entretenimiento/", // done
+  "https://www.mundodeportivo.com/us/estrellas-latinas", // done
+  "https://www.noticiany.com/category/entretenimiento/", // done
+  "https://es.rollingstone.com/categoria/musica/noticias-musica/",
+  "https://www.notistarz.com/categorias/musica/", // tema imagen
+  "https://efe.com/noticias/musica/", // done
+  "https://ellatinodigital.com/categoria/secciones/farandula/", // done
+  "https://www.latingrammy.com/noticias", // error de carga
 ];
 const españa = [
-  'https://www.epe.es/es/temas/musica-5359', // Error 404
-  'https://www.diariodecadiz.es/mapademusicas/', // Error 404
-  'https://es.rollingstone.com/esp/', // VPN
-  'http://noticiasclave.net', // done
-  'https://www.elconfidencial.com/tags/temas/musica-5272/', // done, con falta de datos
-  'https://www.larazon.es/cultura/musica/', // done
-  'https://www.elespanol.com/el-cultural/escenarios/musica/', // done
-  'https://www.elperiodico.com/es/temas/musica-6584', // done
-  'https://www.europapress.es/cultura/musica-00129/', // done
-  'https://www.lavozdealmeria.com/temas/conciertos/1', // done 
-  'https://www.laopiniondemalaga.es/tags/musica/', // done
-  'https://www.diariosur.es/culturas/musica/', // done
-  'https://www.lasprovincias.es/culturas/musica/', // done
-  'https://los40.com/musica', // done
-  'https://los40.com/los40_urban/',
-  'https://www.efeeme.com/agenda/conciertos-agenda/', // done
-  'https://www.cadena100.es/musica', // done
-  'https://masterfm.es/category/musica/', // done
-  'https://as.com/tikitakas/noticias/musica/', // done
-  'https://gladyspalmera.com/actualidad/', // done // tema con la imagen "header > div > a"
-  'https://elpais.com/noticias/musica/', // done
-  'https://www.lasexta.com/temas/musica-1', // done
-  'https://www.telecinco.es/tags/musica', // done
-  'https://www.cosmopolitan.com/es/musica-novedades/',  // done OJO con el link
-  'https://www.esquire.com/es/actualidad/musica/',   // done OJO con el link
-  'https://www.20minutos.es/minuteca/musica-trap/', // done
-  'https://www.laverdad.es/culturas/musica/', // done
-  'https://cadenaser.com/tag/musica/a/', // done
-  'https://www.eldia.es/tags/musica/', // done
-  'https://www.cancioneros.com/in/12/0/actualidad', // revisar estructura
-  'https://elgeneracionalpost.com/noticias/cultura/musica', // done (tema imagen)
+  "https://www.epe.es/es/temas/musica-5359", // Error 404
+  "https://www.diariodecadiz.es/mapademusicas/", // Error 404
+  "https://es.rollingstone.com/esp/", // VPN
+  "http://noticiasclave.net", // done
+  "https://www.elconfidencial.com/tags/temas/musica-5272/", // done, con falta de datos
+  "https://www.larazon.es/cultura/musica/", // done
+  "https://www.elespanol.com/el-cultural/escenarios/musica/", // done
+  "https://www.elperiodico.com/es/temas/musica-6584", // done
+  "https://www.europapress.es/cultura/musica-00129/", // done
+  "https://www.lavozdealmeria.com/temas/conciertos/1", // done
+  "https://www.laopiniondemalaga.es/tags/musica/", // done
+  "https://www.diariosur.es/culturas/musica/", // done
+  "https://www.lasprovincias.es/culturas/musica/", // done
+  "https://los40.com/musica", // done
+  "https://los40.com/los40_urban/",
+  "https://www.efeeme.com/agenda/conciertos-agenda/", // done
+  "https://www.cadena100.es/musica", // done
+  "https://masterfm.es/category/musica/", // done
+  "https://as.com/tikitakas/noticias/musica/", // done
+  "https://gladyspalmera.com/actualidad/", // done // tema con la imagen "header > div > a"
+  "https://elpais.com/noticias/musica/", // done
+  "https://www.lasexta.com/temas/musica-1", // done
+  "https://www.telecinco.es/tags/musica", // done
+  "https://www.cosmopolitan.com/es/musica-novedades/", // done OJO con el link
+  "https://www.esquire.com/es/actualidad/musica/", // done OJO con el link
+  "https://www.20minutos.es/minuteca/musica-trap/", // done
+  "https://www.laverdad.es/culturas/musica/", // done
+  "https://cadenaser.com/tag/musica/a/", // done
+  "https://www.eldia.es/tags/musica/", // done
+  "https://www.cancioneros.com/in/12/0/actualidad", // revisar estructura
+  "https://elgeneracionalpost.com/noticias/cultura/musica", // done (tema imagen)
 ];
 const colombia = [
-  'https://www.eltiempo.com/cultura/musica-y-libros',  // 
-  'https://www.elcolombiano.com/cultura/musica', // Se repite
-  'https://www.decibeles.net/noticias', // OJO con el link
-  'https://www.semana.com/cultura/musica/', // done
-  'https://www.elespectador.com/entretenimiento/musica/', // done
-  'https://www.eluniversal.com.co/farandula', // done
-  'https://occidente.co/secciones/espectaculo/', // done
-  'https://www.elpais.com.co/entretenimiento/', // done
-  'https://colombia.as.com/tikitakas/noticias/musica/', // done
-  'https://intervallenato.com/inicio/category/noticias/', // done
-  'https://www.midiario.co/category/farandula-2/', // done
-  'https://elpilon.com.co/Noticias/el-vallenato/', // done
-  'https://www.elcolombiano.com/cultura/musica', // done
-  'https://www.estiloplay.com.co/category/musica/', // done
-  'https://www.estiloplay.com.co/category/sony-music', // done
-  'https://www.estiloplay.com.co/category/universal-music/', // done
-  'https://www.estiloplay.com.co/category/warner-music/', // done
-  'https://www.estiloplay.com.co/category/onerpm/', // done
-  'https://es.rollingstone.com/col/', // VPN
-  'https://revistadiners.com.co/category/cultura/musica/', // done
-  'https://caracol.com.co/tendencias/entretenimiento/', // done
-  'https://www.shock.co/musica', // done
-  'https://www.shock.co/eventos', // done
-  'https://www.wradio.com.co/tag/musica/a/', // done
+  "https://www.eltiempo.com/cultura/musica-y-libros", //
+  "https://www.elcolombiano.com/cultura/musica", // Se repite
+  "https://www.decibeles.net/noticias", // OJO con el link
+  "https://www.semana.com/cultura/musica/", // done
+  "https://www.elespectador.com/entretenimiento/musica/", // done
+  "https://www.eluniversal.com.co/farandula", // done
+  "https://occidente.co/secciones/espectaculo/", // done
+  "https://www.elpais.com.co/entretenimiento/", // done
+  "https://colombia.as.com/tikitakas/noticias/musica/", // done
+  "https://intervallenato.com/inicio/category/noticias/", // done
+  "https://www.midiario.co/category/farandula-2/", // done
+  "https://elpilon.com.co/Noticias/el-vallenato/", // done
+  "https://www.elcolombiano.com/cultura/musica", // done
+  "https://www.estiloplay.com.co/category/musica/", // done
+  "https://www.estiloplay.com.co/category/sony-music", // done
+  "https://www.estiloplay.com.co/category/universal-music/", // done
+  "https://www.estiloplay.com.co/category/warner-music/", // done
+  "https://www.estiloplay.com.co/category/onerpm/", // done
+  "https://es.rollingstone.com/col/", // VPN
+  "https://revistadiners.com.co/category/cultura/musica/", // done
+  "https://caracol.com.co/tendencias/entretenimiento/", // done
+  "https://www.shock.co/musica", // done
+  "https://www.shock.co/eventos", // done
+  "https://www.wradio.com.co/tag/musica/a/", // done
 ];
 const chile = [
-  'https://www.emol.com/espectaculos/musica/', // error de carga
-  'https://www.latercera.com/etiqueta/musica-culto/', // done
-  'https://www.biobiochile.cl/lista/categorias/musica', // done
-  'https://www.lacuarta.com/musica/', // done
-  'https://www.lacuarta.com/urbana/', // done
-  'https://los40.cl/actualidad/', // done
-  'https://www.musicachilena.cl/v2/noticias/', // done
+  "https://www.emol.com/espectaculos/musica/", // error de carga
+  "https://www.latercera.com/etiqueta/musica-culto/", // done
+  "https://www.biobiochile.cl/lista/categorias/musica", // done
+  "https://www.lacuarta.com/musica/", // done
+  "https://www.lacuarta.com/urbana/", // done
+  "https://los40.cl/actualidad/", // done
+  "https://www.musicachilena.cl/v2/noticias/", // done
 ];
 const rd = [
-  'https://www.diariolibre.com/revista/musica', // done
-  'https://listindiario.com/entretenimiento/musica', // done falta el logo(problemas de internet)
-  'https://diariosocialrd.com/categoria/musica/', // done
-  'https://notidigitalrd.com.do/category/entretenimiento/', // done
-  'https://eldia.com.do/secciones/espectaculos/', // onCheck
-  'https://www.elcaribe.com.do/seccion/gente/a-y-e/', // onCheck
-  'https://www.elperiodico.com.do/secciones/entretenimiento/',
-  'https://eltestigo.do/entretenimiento',
+  "https://www.diariolibre.com/revista/musica", // done
+  "https://listindiario.com/entretenimiento/musica", // done falta el logo(problemas de internet)
+  "https://diariosocialrd.com/categoria/musica/", // done
+  "https://notidigitalrd.com.do/category/entretenimiento/", // done
+  "https://www.elcaribe.com.do/seccion/gente/a-y-e/", // onCheck
+  "https://www.elperiodico.com.do/secciones/entretenimiento/",
+  "https://eltestigo.do/entretenimiento",
+  "https://eldia.com.do/secciones/espectaculos/", // BAD_REQUEST_ERROR
 ];
 const venezuela = [
   "https://www.elnacional.com/musica/", // done
@@ -120,26 +120,26 @@ const venezuela = [
   "https://acn.com.ve/espectaculos/", // done
 ];
 const paraguay = [
-  'https://independiente.com.py/show/' // onCheck
+  "https://independiente.com.py/show/", // done
 ];
 const cr = [
-  'https://www.larepublica.net/seccion/magazine', // done
-  'https://www.crhoy.com/noticias/musica' // done
+  "https://www.larepublica.net/seccion/magazine", // done
+  "https://www.crhoy.com/noticias/musica", // done
 ];
 const uruguay = [
-  'https://www.elpais.com.uy/tvshow', // error de carga
-  'https://www.elobservador.com.uy/tag/musica', // done
+  "https://www.elpais.com.uy/tvshow", // error de carga
+  "https://www.elobservador.com.uy/tag/musica", // done
 ];
 const cuba = [
-  'https://oncubanews.com/category/cultura/musica/', // done
-  'https://magazineampm.com/newness-cuba/', // done
+  "https://oncubanews.com/category/cultura/musica/", // done
+  "https://magazineampm.com/newness-cuba/", // done
 ];
 const bolivia = [
-  'https://www.bolivia.com/entretenimiento/', // vpn
-  'https://www.opinion.com.bo/tags/musica/', // done
+  "https://www.bolivia.com/entretenimiento/", // vpn
+  "https://www.opinion.com.bo/tags/musica/", // done
 ];
 const nicaragua = [
-  'https://www.tn8.tv/category/musica/', // done
+  "https://www.tn8.tv/category/musica/", // done
 ];
 
 const masNoticias = [
@@ -154,7 +154,7 @@ const masNoticias = [
   "https://codigotv.net/category/entretenimiento/ ", // done
   "https://azuaalinstante.com/category/arte-y-espectaculo/ ", // done
   "https://www.disco89fm.com/noticias", // done
-  "https://precision.com.do/category/revista/espectaculos/", // done 
+  "https://precision.com.do/category/revista/espectaculos/", // done
   "https://www.elfarandi.com/musica/", // done
   "https://intervez.com/category/culturaentretenimiento/musica/", // done
   "https://www.ntn24.com/noticias-entretenimiento", // done
@@ -171,92 +171,52 @@ const masNoticias = [
   "https://masvip.com.do/secciones/rincon-urbano/", // done
   "https://masvip.com.do/secciones/quien/", // done
   "https://lifeandstyle.expansion.mx/noticias-musica", // done
-  "https://sglaradio.com/category/noticias/",// ready (just fix image issue on backend)
-  "https://sglaradio.com/category/lanzamientos/",// ready (just fix image issue on backend)
-  "https://www.panasenutah.com/category/entretenimiento/musica/", // onCheck
-  "https://impactolatino.com/entretenimiento/", // onCheck
-  "https://dominicanaaldia.do/Secciones/estilo-de-vida/", // onCheck
-  "https://elcronista.co/cultura", // to test (load error)
+  "https://www.panasenutah.com/category/entretenimiento/musica/", // done
+  "http://tijuanainformativo.info/index.php/noticias-cultura-y-espectaculos-de-tijuana-y-baja-california-y-mexico", // done
+  "https://impactolatino.com/entretenimiento/", // done
+  "https://dominicanaaldia.do/Secciones/estilo-de-vida/", // done
+  "https://sglaradio.com/category/noticias/", // ready (just fix image issue on backend)
+  "https://sglaradio.com/category/lanzamientos/", // ready (just fix image issue on backend)
+  "https://elcronista.co/cultura", // done
   "https://www.revistavea.com.co/musica/", // to test (load error)
   "https://urbanaplayfm.com/category/musica/", // to finnish image (load error)
-  "http://tijuanainformativo.info/index.php/noticias-cultura-y-espectaculos-de-tijuana-y-baja-california-y-mexico", // done
   "https://www.canal26.com/musica", // Error: Unknown pseudo-class :text-base
   "https://www.esquire.com/es/actualidad/musica/", // link issues
-  "https://ultimasnoticia.com/category/entretenimiento/", // link issues 
+  "https://ultimasnoticia.com/category/entretenimiento/", // link issues
   "https://elperiodiquito.com/category/mas/escenario/", // No se deja ver codigo fuente
 ];
 
 const onCheck = [
   {
-    url: "https://www.panasenutah.com/category/entretenimiento/musica/",
-    "ext": false,
-    "link": "a",
-    "main": "li.post-item",
-    "image": "a > img",
-    "title": "div > h2",
-    "content": "div > p",
-    "extImg": false,
-  },
-  {
-    url: "https://impactolatino.com/entretenimiento/",
-    "ext": false,
-    "link": "div > a",
-    "main": "div.td-big-grid-post",
-    "image": "div > a > img",
-    "title": "div > div > div > h3 > a",
-    "content": "div > div > p",
-    "extImg": false,
-  },
-  {
     url: "https://sglaradio.com/category/noticias/",
-    "ext": false,
-    "link": "div > figure > a",
-    "main": "div.post-col",
-    "image": "div > figure > a",
-    "title": "div > header > h2 > a",
-    "content": "div > div > p",
-    "extImg": true,
-    "cssImage": true,
-    "cssImageAttr": "style",
-    "cssImageExtraText": true,
+    ext: false,
+    link: "div > figure > a",
+    main: "div.post-col",
+    image: "div > figure > a",
+    title: "div > header > h2 > a",
+    content: "div > div > p",
+    extImg: true,
+    cssImage: true,
+    cssImageAttr: "style",
+    cssImageExtraText: true,
   },
   {
-    url: "https://dominicanaaldia.do/Secciones/estilo-de-vida/",
-    "ext": false,
-    "link": "div > a",
-    "main": "article.list-post",
-    "image": "div > a > img",
-    "title": "div > div > h2 > a",
-    "content": "div > div > p",
-    "extImg": true,
+    url: "https://www.revistavea.com.co/musica/",
+    ext: false,
+    link: "",
+    main: "",
+    title: "",
+    image: "",
+    content: "",
+    extImg: false,
+    // cssImage: true,
+    // cssImageAttr: "data-src",
+    // cssImageExtraText: false,
   },
-  {
-    url: "https://eldia.com.do/secciones/espectaculos/",
-    "ext": false,
-    "link": "a",
-    "main": "article.post",
-    "title": "div > header > h3 > a",
-    "image": "a > div > div > img",
-    "content": "",
-    "extImg": false,
-  },
-  {
-    url: "https://independiente.com.py/show/",
-    "ext": false,
-    "link": "div > div > div > a",
-    "main": "div.tdb_module_loop.td_module_wrap.td-animation-stack.td-cpt-post",
-    "title": "div > div > h3 > a",
-    "image": "div > div > div > a > span",
-    "content": "div.td-excerpt",
-    "extImg": false,
-    "cssImage": true,
-    "cssImageAttr": "data-img-url",
-    "cssImageExtraText": false,
-  }
 ];
 
 const tester = "https://bot.sannysoft.com";
 
-// module.exports = { add_media, add_media_data };
+module.exports = { onCheck };
 
 // background-image: url('https://gladyspalmera.com/wp-content/uploads/2024/07/Dayme-Visita-984x620.jpg')
